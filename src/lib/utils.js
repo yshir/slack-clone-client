64cd0dd235c127bb4d722593fb578e652426fd65
yshir <yshir0020@gmail.com>
2020-07-31 15:15:46 +0900

    refactor

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,16 +3,17 @@ dayjs.locale('ja')
 
 export const formatDateTime = datetime => {
   const date = dayjs(datetime).format('YYYYMMDD')
+  const time = dayjs(datetime).format('h:mm A')
 
   const dateToday = dayjs().format('YYYYMMDD')
   if (date === dateToday) {
-    return 'Today at ' + dayjs(datetime).format('h:mm A')
+    return `Today at ${time}`
   }
 
   const dateYesterday = dayjs().add(-1, 'day').format('YYYYMMDD')
   if (date === dateYesterday) {
-    return 'Yesterday at ' + dayjs(datetime).format('h:mm A')
+    return `Yesterday at ${time}`
   }
 
-  return dayjs(datetime).format('YYYY-MM-DD h:mm A')
+  return `${date} ${time}`
 }
