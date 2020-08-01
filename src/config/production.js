077e76a5a0cd3a08057debb0dba5d4d90a463af8
yshir <yshir0020@gmail.com>
2020-08-01 17:08:33 +0900

    set-production-config

--- src/config/production.js
+++ src/config/production.js
@@ -1,9 +1,9 @@
 export default {
   api: {
-    root_url: '',
+    root_url: 'https://slack-clone-api-xyz.herokuapp.com/v1',
   },
   socket: {
-    endpoint: '',
+    endpoint: 'https://slack-clone-api-xyz.herokuapp.com',
     events: {
       update_message: 'update-message',
     },
